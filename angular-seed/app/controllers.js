40c217c754c2b2dd3acb452fe31cd20b397f6ea2
rodrigoandrade <rodrigo.oliveira.and@gmail.com>
2016-10-16 22:09:59 -0200

    Building Single Page Web Apps with AngularJS

---
+++ angular-seed/app/controllers.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('todoApp.controller', [])
+	.controller('todoController', ["$scope", "$filter", function($scope, $filter) {
+		$scope.newTask="";
+
+		$scope.pendingCount = 3;
+
+		$scope.taskList = [
+			{description: "Buy airplane tickets", done:false},
+			{description: "Make hotel reservations", done:false},
+			{description: "Chill", done:false}
+		];
+
+		$scope.addTodo = function() {
+			$scope.taskList.push({description:$scope.newTask, done:false});
+			$scope.newTask="";
+		};
+
+		$scope.deleteTodo = function(index) {
+			$scope.taskList.splice(index, 1);
+		}
+		
+		$scope.$watch('taskList', function() {
+			$scope.pendingCount = $filter('filter')($scope.taskList, {done:false}).length;
+		}, true);
+
+		$scope.clearCompleted = function() {
+			$scope.taskList = $filter('filter')($scope.taskList, {done:false});
+		};
+
+	}]);
